6b28cd18f2c76a86f7ff817661c54bfe3eed0c54
Brandon U <unzueta.brandon@gmai.com>
2020-09-01 20:15:35 -0700

    updateValues function

--- script.js
+++ script.js
@@ -29,6 +29,29 @@ function addTransactionDOM(transaction) {
   list.appendChild(item);
 }
 
+// UPDATE BALANCE INCOME AND EXPENSE
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
+
+  money_plus.innerText = `$${income}`;
+
+  money_minus.innerText = `$${expense}`;
+}
+
 function init() {
   list.innerHTML = "";
 
@@ -36,3 +59,4 @@ function init() {
 }
 
 init();
+updateValues();
